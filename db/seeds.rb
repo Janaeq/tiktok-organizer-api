20f53c646df207df9ef078c5e60210c9414fd339
Janae Quinones <janaemassa@gmail.com>
2021-03-30 02:24:42 -0400

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# CATEGORIES
+coffee = Category.create(name: "Coffee")
+
+# VIDEOS
+sample_video = Video.create(url: "https://www.tiktok.com/@caffeinication/video/6940339533191957765", category_id: 1)
