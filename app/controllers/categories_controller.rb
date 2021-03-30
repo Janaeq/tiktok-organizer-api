28f5b1e7841c5453fa7ed7cdab523f9a215e6d76
Janae Quinones <janaemassa@gmail.com>
2021-03-30 03:12:02 -0400

    index and show json

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,15 @@
 class CategoriesController < ApplicationController
+    def index
+        categories = Category.all
+        render json: CategorySerializer.new(categories).to_serialized_json
+    end
+
+    def show
+        category = Category.find_by(id: params[:id])
+        if category
+            render json: CategorySerializer.new(category).to_serialized_json
+        else
+            render json: {message: 'Category does not exist'}
+        end
+    end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,14 @@
 class VideosController < ApplicationController
     def index
         videos = Video.all 
-        render json: videos
+        render json: videos, except: [:created_at, :updated_at]
+    end
+
+    def show
+        video = Video.find_by(id: params[:id])
+        if video
+            render json: video, except: [:created_at, :updated_at]
+        else
+            render json: {message: 'video does not exist'}
     end
 end
