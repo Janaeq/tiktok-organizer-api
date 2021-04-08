8731f73e2a5e6b3afe228009a1505e6ad3d3932f
Janae Quinones <janaemassa@gmail.com>
2021-04-08 02:08:13 -0400

    get thumbnail and embed HTML from tiktok oembed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
+require 'httparty'
 class ApplicationController < ActionController::API
+    include HTTParty
+    @@uri = 'https://www.tiktok.com/oembed?url='
+
+    def get_thumbnail(url)
+        response = HTTParty.get(@@uri + url)
+        json = JSON.parse(response.body)
+        return json['thumbnail_url']
+    end
+
+    def get_embedded_html(url) 
+        response = HTTParty.get(@@uri + url)
+        json = JSON.parse(response.body)
+        return json['html']
+    end
 end
